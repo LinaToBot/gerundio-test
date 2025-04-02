1775d1c9d8199f46291d7357ecc6d1f461976550
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-01 19:01:35 -0600

    feat(MainLayout.tsx): MainLayout.tsx created and App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.scss";
 import Footer from "./components/Footer/Footer";
+import MainLayout from "./components/MainLayout/MainLayot";
 import Header from "./components/Header/Header";
+import "./App.scss";
 
 function App() {
   return (
     <>
       <Header />
-      <main>Main</main>
+      <MainLayout />
       <Footer />
     </>
   );
---
+++ src/components/MainLayout/MainLayot.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "react";
+import "./styles.scss";
+
+const MainLayout = (): JSX.Element => {
+  return (
+    <main className="main-layout">
+      <section className="sections"></section>
+      <section className="sections"></section>
+      <section className="sections"></section>
+      <section className="sections"></section>
+    </main>
+  );
+};
+
+export default MainLayout;
