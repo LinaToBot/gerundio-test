e3c58fd95601e4f8ff38afbc218d2c35f8ad5dc0
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-02 00:12:47 -0600

    feat(ProductSection.tsx): desktop styles added to ProductSection.tsx

--- src/components/ProductSection/ProductSection.tsx
+++ src/components/ProductSection/ProductSection.tsx
@@ -3,23 +3,28 @@ import "./styles.scss";
 
 const ProductSection = () => {
   return (
-    <>
-      <div className="titles">
-        <p>Smart Insurance</p>
-        <p>Solutions for a Worry</p>
-        <p>Free Life</p>
+    <section className="sections">
+      <div>
+        <div className="titles">
+          <p>Smart Insurance</p>
+          <p>Solutions for a Worry</p>
+          <p>Free Life</p>
+        </div>
+        <img src={img} alt="people" id="mobile-img" />
+        <div id="content-product">
+          <p className="paragraph">
+            Stay protected with flexible coverage that safeguards your health,
+            family, and assets. Choose the right plan and enjoy financial peace
+            of mind.
+          </p>
+          <div className="product-btn">
+            <button>Get a Free Quote</button>
+            <button>View Our Plans</button>
+          </div>
+        </div>
       </div>
-      <img src={img} alt="people"></img>
-      <p className="paragraph">
-        Stay protected with flexible coverage that safeguards your health,
-        family, and assets. Choose the right plan and enjoy financial peace of
-        mind.
-      </p>
-      <div className="product-btn">
-        <button>Get a Free Quote</button>
-        <button>View Our Plans</button>
-      </div>
-    </>
+      <img src={img} alt="people" id="desktop-img" />
+    </section>
   );
 };
 
