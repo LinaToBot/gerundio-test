3abd4e035fc2e67c30b8fa9e11e07c4eaf19f986
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-01 18:17:23 -0600

    feat(footer): footer created and styles added to mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <>
       <Header />
       <main>Main</main>
-      <footer>Footer</footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+    <footer className="customized-footer">
+      <ul>
+        <li>
+          <a href="">Terms & Conditions</a>
+        </li>
+        <li>
+          <a href="">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="">Licensing</a>
+        </li>
+      </ul>
+      <div className="divisor-line"></div>
+      <p>© 2025 Shield Plus. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
