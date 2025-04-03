1425a36686b9b2171cb61682822f1d239542c1c3
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-03 04:17:45 -0600

    feat(styles): mobile styles added to InstructionsSection

--- src/components/InstructionsSection/InstructionsSection.tsx
+++ src/components/InstructionsSection/InstructionsSection.tsx
@@ -31,7 +31,7 @@ const InstructionsSection: React.FC = () => {
         loading="lazy"
       />
 
-      <div>
+      <div className="instructions">
         {isLargeScreen && (
           <div className="desktop-paragraphs">
             <p>Getting Insured Is Simple & Fast</p>
@@ -42,6 +42,30 @@ const InstructionsSection: React.FC = () => {
             </p>
           </div>
         )}
+
+        <InstructionClause
+          number="01 - "
+          title="Choose a Plan"
+          instruction="Browse through our range of insurance plans tailored for your needs. Whether you need health, life, car, home, or travel insurance, we have a plan that fits your lifestyle and budget."
+        />
+
+        <InstructionClause
+          number="02 - "
+          title="Get a Free Quote"
+          instruction="Fill out a quick form with basic details, and our system will generate a personalized quote instantly. No commitments, no hidden charges—just a transparent breakdown of your insurance options."
+        />
+
+        <InstructionClause
+          number="03 - "
+          title="Apply Online"
+          instruction="Once you’ve chosen your plan, complete the application online in a few simple steps. Our digital process ensures secure and hassle-free enrollment—no lengthy paperwork required."
+        />
+
+        <InstructionClause
+          number="04 - "
+          title="Get Covered Instantly"
+          instruction="Once your application is reviewed and approved, your policy becomes active immediately. You’ll receive a digital copy of your insurance documents, and you’re fully protected from day one."
+        />
       </div>
     </section>
   );
