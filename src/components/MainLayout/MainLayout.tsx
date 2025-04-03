4644e3228d1e5168d41d90032606250e0de78883
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-03 10:05:23 -0600

    feat(MainLayout): clean code

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -19,8 +19,6 @@ const MainLayout: React.FC = () => {
       <ProductSection />
       <BenefitsSection />
       <InstructionsSection />
-      <section className="sections"></section>
-      <section className="sections"></section>
     </main>
   );
 };
