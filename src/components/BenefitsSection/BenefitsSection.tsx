655f77874ab79bc7ae3afa876a453afa48a4ebf5
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-02 18:31:49 -0600

    feat(BenefitsSection.tsx): BenefitsSection component and styles file created, and svg added

---
+++ src/components/BenefitsSection/BenefitsSection.tsx
@@ -0,0 +1,54 @@
+import CardInfo from "../CardInfo/CardInfo";
+import plansIcon from "../../assets/plans.svg";
+import supportIcon from "../../assets/support.svg";
+import quickIcon from "../../assets/quick.svg";
+import secureIcon from "../../assets/secure.svg";
+import coverageIcon from "../../assets/coverage.svg";
+import premiumIcon from "../../assets/premium.svg";
+import "./styles.scss";
+
+const BenefitsSection: React.FC = () => {
+  return (
+    <section className="sections">
+      <div>
+        <p>Why Thousands Trust Us </p>
+        <p>for Their Insurance Needs</p>
+        <p>for Their Insurance Needs</p>
+      </div>
+      <div>
+        <CardInfo
+          imgPath={plansIcon}
+          benefit="Custom Plans Available"
+          information="Select from a variety of plans designed to fit your lifestyle, needs, and budget, ensuring the right coverage for you and your family."
+        />
+        <CardInfo
+          imgPath={supportIcon}
+          benefit="Support Anytime Anywhere"
+          information="Get professional assistance 24/7 from our dedicated team, ready to guide you through policies, claims, and insurance solutions anytime."
+        />
+        <CardInfo
+          imgPath={quickIcon}
+          benefit="Quick Claims Process"
+          information="Enjoy a fast, simple, and transparent claims process with minimal paperwork, ensuring swift approvals and hassle-free payouts."
+        />
+        <CardInfo
+          imgPath={secureIcon}
+          benefit="Secure and Private"
+          information="Your personal data and transactions are fully protected with the latest encryption, ensuring security and confidentiality at all times."
+        />
+        <CardInfo
+          imgPath={coverageIcon}
+          benefit="Global Coverage Plans"
+          information="Stay protected wherever you go with worldwide insurance coverage, offering peace of mind for both travel and daily life."
+        />
+        <CardInfo
+          imgPath={premiumIcon}
+          benefit="Affordable Premium Options"
+          information="Get high-value insurance plans with transparent pricing, no hidden fees, and competitive rates tailored to your financial needs."
+        />
+      </div>
+    </section>
+  );
+};
+
+export default BenefitsSection;
