ef17d6481f9e2f43e02cea7c3bce955d3c77dea5
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-04-02 18:26:49 -0600

    feat(CardInfo.tsx): CardInfo component created and styles file created

---
+++ src/components/CardInfo/CardInfo.tsx
@@ -0,0 +1,24 @@
+import "./styles.scss";
+type CardInfoProps = {
+  imgPath: string;
+  information: string;
+  benefit: string;
+  cssClass?: string;
+};
+
+const CardInfo: React.FC<CardInfoProps> = ({
+  imgPath,
+  information,
+  benefit,
+  cssClass,
+}) => {
+  return (
+    <div>
+      <img src={imgPath} alt={`${benefit}-icon`} className={cssClass} />
+      <p>{benefit}</p>
+      <p>{information}</p>
+    </div>
+  );
+};
+
+export default CardInfo;
